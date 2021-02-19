59fc71f18662981b73b7edb5a1bf8ef7bd204594
Samet Toylan <toylansamet@gmail.com>
2021-02-20 01:07:19 +0300

    initial commit

--- pet-clinic-web/src/test/java/stoylan/springframework/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/stoylan/springframework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -2,8 +2,9 @@ package stoylan.springframework.springpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import stoylan.springframework.springpetclinic.web.SpringPetClinicApplication;
 
-@SpringBootTest(classes = SpringPetClinicApplicationTests.class)
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
     @Test
