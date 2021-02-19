944acf8d78e76c78c3485aa99f9ce14cec69d025
Samet Toylan <toylansamet@gmail.com>
2021-02-19 23:33:02 +0300

    Adding multi-module maven builds

--- pet-clinic-web/src/test/java/stoylan/springframework/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/stoylan/springframework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,15 +1,14 @@
-package springpetclinic;
+package stoylan.springframework.springpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 class SpringPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
